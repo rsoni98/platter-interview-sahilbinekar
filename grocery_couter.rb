8bc5534c608f085f4af1ed75638b99886baf72ee
sahil-binekar <sahilbinekar@gmail.com>
2026-02-03 00:21:58 +0530

    Grocery calculation initial commit

--- grocery_couter.rb
+++ grocery_couter.rb
@@ -1,6 +1,37 @@
+require "active_support/hash_with_indifferent_access"
 class PriceCalculator
   def initialize
-    
+    get_input
+  end
+    STORE_ITEMS = {
+      milk:{
+        unit_price: 3.97,
+        sale: true,
+        qt: 2,
+        sale_price: 5.00
+      },
+      bread: {
+        unit_price: 2.17,
+        sale: true,
+        qt: 3,
+        sale_price: 6.00
+      },
+      banana: {
+        unit_price: 0.99,
+        sale: false,
+        qt: 0,
+        sale_price: 0
+      },
+      apple: {
+        unit_price: 0.89,
+        sale: false,
+        qt: 0,
+        sale_price: 0
+      }
+    }
+
+  def get_input
+    @input_list = gets.chomp
   end
   
 end
