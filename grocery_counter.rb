5807ef3ab2dc4013be1164577fe20205484b11d0
sahil-binekar <sahilbinekar@gmail.com>
2026-02-03 13:21:36 +0530

    Implemented the Calculation methods for total price and sale total price and also created the sorted hash for the final table representation

--- grocery_counter.rb
+++ grocery_counter.rb
@@ -16,6 +16,8 @@ class PriceCalculator
   def initialize
     get_input
     sale_product_group
+    calculate_product_total
+    calculate_sale_product_total
   end
 
     STORE_ITEMS = {
@@ -49,10 +51,6 @@ class PriceCalculator
     @input_list = gets.chomp
   end
 
-  def calculate_sale_total
-
-  end
-
   def product_group
     sorted_list = Hash.new(0)
 
@@ -79,6 +77,41 @@ class PriceCalculator
 
     sale_sorted_list
   end
+
+  def calculate_sale_product_total
+    sale_product_list = sale_product_group
+    sale_total_cost = 0
+    final_table_hash = {}
+
+    sale_product_list.flat_map(&:to_a).each do |item|
+      if STORE_ITEMS[item[0]]["sale"] && (STORE_ITEMS[item[0]]["qt"] == item[1])
+        final_table_hash[item[0]] ||= {"qt" => 0, "price" => 0 }
+        sale_total_cost = sale_total_cost + STORE_ITEMS[item[0]]["sale_price"]
+        final_table_hash[item[0]]["qt"] += item[1]
+        final_table_hash[item[0]]["price"]+= STORE_ITEMS[item[0]]["sale_price"]
+      else
+        final_table_hash[item[0]] ||= {"qt" => 0, "price" => 0 }
+        sale_total_cost = sale_total_cost + (STORE_ITEMS[item[0]]["unit_price"] * item[1])
+        final_table_hash[item[0]]["qt"] += item[1]
+        final_table_hash[item[0]]["price"]+= (STORE_ITEMS[item[0]]["unit_price"] * item[1])
+      end
+    end
+    # # p sale_total_cost
+    # p final_table_hash
+    # p "\n" 
+    # p sale_product_list
+  end
+        # binding.pry
+
+  def calculate_product_total
+    product_list = product_group
+    total_cost = 0
+    product_list.each do |item|
+      total_cost = total_cost + (STORE_ITEMS[item[0]]["unit_price"] * item[1])
+    end
+    total_cost
+  end
+
 end
 
 PriceCalculator.new()
